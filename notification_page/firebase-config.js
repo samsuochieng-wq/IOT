28aa61cb371e23693fe745b4543b53f942770554
samsuochieng-wq <samsuochieng@gmail.com>
2026-07-10 06:43:31 +0300

    Update firebase-config.js

--- notification_page/firebase-config.js
+++ notification_page/firebase-config.js
@@ -1,26 +1,19 @@
-// Firebase SDK imports
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import { getMessaging } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-messaging.js";
+// Firebase config for IoT project
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBw0eieldINjNpKRhzJwFr4RDxPKXlDhj4",
-    authDomain: "iot01-3f1ea.firebaseapp.com",
-    databaseURL: "https://iot01-3f1ea-default-rtdb.firebaseio.com",
-    projectId: "iot01-3f1ea",
-    storageBucket: "iot01-3f1ea.firebasestorage.app",
-    messagingSenderId: "660894621070",
-    appId: "1:660894621070:web:670de3e001e7205ef498df",
-    measurementId: "G-SFGBT9JKYK"
+  apiKey: "AIzaSyBw0eieldINjNpKRhzJwFr4RDxPKXlDhj4",
+  authDomain: "iot01-3f1ea.firebaseapp.com",
+  databaseURL: "https://iot01-3f1ea-default-rtdb.firebaseio.com",
+  projectId: "iot01-3f1ea",
+  storageBucket: "iot01-3f1ea.firebasestorage.app",
+  messagingSenderId: "660894621070",
+  appId: "1:660894621070:web:670de3e001e7205ef498df",
+  measurementId: "G-SFGBT9JKYK"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Cloud Messaging
-const messaging = getMessaging(app);
-
-// Replace with YOUR Web Push Certificate
-const vapidKey = "BPhWkuS8U1-EY3oPStGKYYhHf1_QSEAcifE3AyQDf4cgyUrb31hGsp8ZEnggjl24rIDMSdGwc9zfsKsDcImlHx8";
-
-export { app, messaging, vapidKey };
+export const db = getDatabase(app);
+export const auth = getAuth(app);
